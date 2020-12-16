8868db89c052e5257df768cf8840a45fe27300c4
syitilv <syitiliviani3@gmail.com>
2020-12-16 17:40:32 +0700

    done

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+//routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var farmerRouter = require('./routes/farmerRoute');
+var buyersRouter = require('./routes/buyersRoute');
 
 var app = express();
+
+//database
+var url = 'mongodb://localhost:27017/db_betani';
+mongoose.connect(url);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +28,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//API
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/petani', farmerRouter);
+
+app.use('/buyers', buyersRouter);
+app.use('/buyers/:buyerId', buyersRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
