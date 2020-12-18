6579d95484e69015afe5d90beb69ca60d7d33e72
niningparwati <niningparwati04@gmail.com>
2020-12-18 17:50:59 +0700

    update api weather

--- routes/weather.js
+++ routes/weather.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const cuaca = require('../weather');
+const request = require('request');
+const dotenv = require('dotenv').config();
 
 const weatherRouter = express();
-weatherRouter.use(bodyParser.json());
-weatherRouter.use(bodyParser.urlencoded({ extended: false }));
-weatherRouter.use(cors());
 
-weatherRouter.get('/', cuaca.index);
+weatherRouter.route('/:kota')
+.get((req, res, next) => {
+    const address = req.params.kota;
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${address},id&appid=${process.env.API_KEY}`;
+
+    request(url, (err, q, body) => { 
+        const data = JSON.parse(body);
+        res.json({
+            city: data.name,
+            main: data.main,
+            weather: data.weather[0].main,
+            description: data.weather[0].description,
+            wind: 'speed = '+data.wind.speed+', deg = '+data.wind.deg,
+            rain : data.rain,
+            sunrise: data.sys.sunrise,
+            sunset: data.sys.sunset
+        })
+    });
+})
 
 module.exports = weatherRouter;
